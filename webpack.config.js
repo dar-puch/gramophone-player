4968d4006909b070694eadbcfc9896d7d0ee8c13
Daria <DellLatitude1@MacBook-Pro.local>
2020-05-31 14:25:34 +0200

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,51 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/player.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    main: "./src/player.js",
+    styles: "./src/style.css",
   },
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
-    },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      {
+        test: /\.(png|jpe?g|gif|woff|woff2|mp3)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/indexTemplate.html" }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new FixStyleOnlyEntriesPlugin(),
+    new OptimizeCSSAssetsPlugin({}),
+    new CopyPlugin({
+      patterns: [{ from: "./src/images/", to: "images/" }],
+    }),
   ],
 };
